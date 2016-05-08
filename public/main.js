68148b8d535303ec8472b954dd0175d3cde5de06
Anton Bashev <a1a11a111@yandex.ru>
2016-05-08 19:07:57 +0300

    getSession & setSession

--- public/main.js
+++ public/main.js
@@ -1,21 +1,29 @@
 window.onload = function() {
 	socket = io.connect('http://127.0.0.1:8081');
-
+//JSON.parse(JSON.stringify(a))
 	socket.on('connect', function () {
 		socket.on('message', function (msg) {
-			document.querySelector('#log').innerHTML += "[" + msg.type + "] " + msg.data;
+			document.querySelector('#log').innerHTML += JSON.stringify(msg) + "<br />";
 			document.querySelector('#log').scrollTop = document.querySelector('#log').scrollHeight;
 		});
 
 		document.querySelector('#input').onkeypress = function(e) {
 			if (e.which == '13') {
-				socket.send((document.querySelector('#input').value));
-				document.querySelector('#input').value = '';
+				socket.send({
+					action: 'getSession',
+					data: {}
+				});
 			}
 		};
 		document.querySelector('#send').onclick = function() {
-			socket.send((document.querySelector('#input').value));
-			document.querySelector('#input').value = '';
+			if (e.which == '13') {
+				socket.send({
+					action: 'getSession',
+					data: {}
+				});
+			}
+			//socket.send((document.querySelector('#input').value));
+			//document.querySelector('#input').value = '';
 		};		
 	});
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 var io = require('socket.io').listen(8081); 
 var Client = require('mariasql');
+var crypto = require('crypto');
 
 var c = new Client({ 
 	host: '127.0.0.1', 
@@ -8,21 +9,40 @@ var c = new Client({
 	password: 'root' 
 });
 
+var clients = [];
+var numClients = 0;
+
 io.sockets.on('connection', function (socket) {
 	console.log('New connection');
+	c.query('USE paint;');
+
+	clients.
 
 	socket.on('message', function (query) {
-		console.log('New message ' + query);
+		console.log('New message ' + query.action);
 
-		c.query(query, function(err, rows) { 
-			if (err) {
-				socket.json.send({ type: 'error', data: err });
-				console.log(err);
-			} else {
-				socket.json.send({ type: 'data', data: rows });
-				console.log(rows);
-			}
-		});
+		if (query.action == 'getSession') {
+			var session = crypto.createHash('md5').update(Date.now() + '').digest('hex');
+			c.query('INSERT INTO sessions VALUES (null, \'' + session + '\', now());', function (err, rows) {
+				if (err) {
+					socket.json.send({ action: 'getSession', type: 'error', data: err });
+				} else {
+					socket.json.send({ action: 'getSession', type: 'data', data: { session: session } });
+				}
+			});
+		} else if (query.action == 'setSession') {
+			c.query('SELECT COUNT(*) as count FROM sessions WHERE code = \'' + query.data.session + '\';', function (err, rows) {
+				if (err) {
+					socket.json.send({ action: 'setSession', type: 'error', data: { type: 'DB', error: err } });
+				} else {
+					if (rows[0]['count'] == 0) {
+						socket.json.send({ action: 'setSession', type: 'error', data: { type: 'No session' } });
+					} else {
+						socket.json.send({ action: 'setSession', type: 'data', data: { type: 'Ok' } });
+					}
+				}
+			});
+		}
 	});
 	socket.on('disconnect', function() {
 		c.end();
