24c7eb02613d15900bef85f66dbd3377721e9827
Anton Bashev <a1a11a111@yandex.ru>
2016-05-08 15:51:29 +0300

    Init script for Ubuntu

---
+++ init.sh
@@ -0,0 +1,13 @@
+# Installing main packages
+sudo apt-get install nodejs
+sudo apt-get install npm
+
+# WebSocket
+npm install socket.io
+
+# Simple 
+npm install node-static
+
+# Maria DB
+sudo apt-get install mariadb-server
+npm install node-mariadb
